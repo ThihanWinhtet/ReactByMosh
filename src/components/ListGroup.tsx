167075b9de871bd34c054e08a5eb0d648bb0a86b
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-20 13:24:20 +0630

    Handle Events.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,21 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let list = ["New York", "Paris", "Tokyo", "Koyto"];
-  list = [];
+  //   list = [];
+  const handleClick = (event : MouseEvent) => {console.log(event);
+  }
   return (
     <>
       <h1>Hello Lists</h1>
       {list.length === 0 && <p>no items at all</p>}
       <ul className="list-group">
         {list.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
