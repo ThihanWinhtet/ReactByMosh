3fc5610f502fa5195c1b62906dbf715fd7cae76d
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-20 15:07:48 +0630

    Exercise Building a Button Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+
+Button;
 
 function App() {
-
   // let list = ["New York", "Paris", "Tokyo", "Koyto"];
   return (
     <div>
-      
+      <Button color="danger">hello button</Button>
+
       {/* <ListGroup items={list} heading="Cities" onSelectedIndex={(item)=> item} onSelected={(item)=> console.log(item, 'lll')}/> */}
     </div>
   );
