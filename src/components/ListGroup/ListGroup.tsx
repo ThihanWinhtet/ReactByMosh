01154d05b31d0d191614df9d04ce35702533d9ed
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-20 23:43:46 +0630

    css modue.

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
-import './ListGroup.css'
+import styles from "./ListGroup.module.css";
 
 interface Props {
   items: string[];
   heading: string;
-  onSelected : (item : string) => void;
-  onSelectedIndex : (item : number) => number;
+  onSelected: (item: string) => void;
+  onSelectedIndex: (item: number) => number;
 }
 
-function ListGroup({ items, heading, onSelected, onSelectedIndex}: Props) {
+function ListGroup({ items, heading, onSelected, onSelectedIndex }: Props) {
   let [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>{heading}</h1>
-      <ul className="list-group">
+      <ul className={[styles.listGroup, styles.container].join(' ')}>
         {items.map((item, index) => (
           <li
             className={
@@ -23,9 +23,9 @@ function ListGroup({ items, heading, onSelected, onSelectedIndex}: Props) {
             }
             key={item}
             onClick={() => {
-                onSelected(item);
-                let inde = onSelectedIndex(index);
-                
+              onSelected(item);
+              let inde = onSelectedIndex(index);
+
               setSelectedIndex(inde);
             }}
           >
