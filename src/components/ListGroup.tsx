0714cf39fcfee1fcca31048333d1d97d4d011058
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-20 11:55:24 +0630

    Conditional Rendering.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,10 @@
 function ListGroup() {
   let list = ["New York", "Paris", "Tokyo", "Koyto"];
+  list = [];
   return (
     <>
       <h1>Hello Lists</h1>
+      {list.length === 0 && <p>no items at all</p>}
       <ul className="list-group">
         {list.map((item) => (
           <li className="list-group-item" key={item}>
