51fe43254d77047646624409e5c1736875573035
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-22 09:34:34 +0630

    Form error handling.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,27 @@
 // import { FormEvent, useRef, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
 
 function Form() {
 
-  let {register, handleSubmit} = useForm();
-  
-  let handleSubmition = (data : FieldValues )=>{
+  let scheme = z.object({
+    name: z.string().min(3),
+    age: z.string().min(18),
+  });
+
+  let {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver : zodResolver(scheme)});
+
+
+  type FormData = z.infer<typeof scheme>;
+
+  let handleSubmition = (data: FieldValues) => {
     console.log(data);
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit(handleSubmition)}>
@@ -15,13 +29,25 @@ function Form() {
         <label htmlFor="" className="form-label">
           Name
         </label>
-        <input {...register('name')} type="text" className="form-control" />
+        <input {...register("name")} type="text" className="form-control" />
+        {errors.name && (
+          <p className="text-danger mt-2">{errors.name.message}</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="" className="form-label">Age</label>
-        <input {...register('age')} type="number" className="form-control" />
+        <label htmlFor="" className="form-label">
+          Age
+        </label>
+        <input {...register("age")} type="number" className="form-control" />
+        {errors.age && (
+          <p className="text-danger mt-2">{errors.age.message}</p>
+        )}
+
+        {/* { errors.name?.type == 'required' && <p className="text-danger mt-2">The name field is required.</p>} */}
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 }
