0dda8057de6ea1e2d078b8cdd7ab94d0f638225c
Thihan Winhtt <thihanwinhtet1999@gmail.com>
2023-06-23 13:29:58 +0630

    axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,109 @@
-
+import axios from "axios";
 import { useEffect, useState } from "react";
-import ProductList from "./components/ProductList/ProductList";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  let [users, setUser] = useState<User[]>([]);
+  let [error, setError] = useState("");
+  let [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUser(res.data), setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message), setLoading(false);
+      });
+  }, []);
 
+  // delete
+  let deleteUser = (user: User) => {
+    const originalUser = [...users];
+    setUser(users.filter((u) => u.id != user.id));
 
-  let [category, setCategory] = useState('');
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message), setUser(originalUser);
+      });
+  };
 
-  useEffect(()=>{
-    document.title = 'my app'
-  });
+  // add
+  let addUser = () => {
+    let newUser = { id: 0, name: "Mosh" };
+    let originalUser = [...users];
+    setUser([newUser, ...users]);
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then((res) => {
+        setUser([res.data, ...users]);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setUser(originalUser);
+      });
+  };
+
+  // update
+  let update = (user: User) => {
+    let originalUser = [...users];
+    let updatedUser = { ...user, name: user.name + "!" };
+    setUser(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setUser(originalUser), setError(err.message);
+      });
+  };
 
   return (
     <div>
-      <select name="" id="" className="form-control" onChange={(event)=>setCategory(event.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="HouseHold">House Hold</option>
-      </select>
-      <ProductList category={category}></ProductList>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-4" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}{" "}
+            <div>
+              <button
+                className="btn btn-primary mx-1"
+                onClick={() => {
+                  update(user);
+                }}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  deleteUser(user);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 export default App;
